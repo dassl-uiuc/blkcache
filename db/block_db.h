db07bfd50b8fefa6650ae5eb17e2f5c4f50d2d6d
maknee <zhu109@illinois.edu>
2023-11-07 16:04:23 -0700

    trunc

--- db/block_db.h
+++ db/block_db.h
@@ -40,11 +40,17 @@ public:
       exit(EXIT_FAILURE);
     }
     lseek(fd, 0, SEEK_SET);
-    if (ftruncate(fd, storage_size)) {
-      perror("ftruncate");
-      exit(EXIT_FAILURE);
-    }
-    auto remaining = storage_size;
+
+    lseek(fd, storage_size, SEEK_SET);
+    write(fd, buf, 1);
+    lseek(fd, 0, SEEK_SET);
+
+    // if (ftruncate(fd, storage_size)) {
+    //   perror("ftruncate");
+    //   exit(EXIT_FAILURE);
+    // }
+
+    // auto remaining = storage_size;
     // while (remaining > 0)
     // {
     //     auto remaining_size = std::min(MAX_POSIX_MEMALIGN_SIZE, remaining);
