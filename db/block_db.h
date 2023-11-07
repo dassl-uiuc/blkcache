a03df0ce4d7d40f634e0a109a1ddf44050b20d10
maknee <zhu109@illinois.edu>
2023-11-07 15:43:46 -0700

    Wrong sync?

--- db/block_db.h
+++ db/block_db.h
@@ -34,8 +34,8 @@ public:
     num_entries = block_cache_config.db.block_db.num_entries;
     storage_size = num_entries * block_size;
     char *buf = nullptr;
-    int64_t remaining = storage_size;
-    constexpr std::size_t MAX_POSIX_MEMALIGN_SIZE = 4096u;
+    auto remaining = storage_size;
+    constexpr std::size_t MAX_POSIX_MEMALIGN_SIZE = 1024u * 1024u;
     if (posix_memalign((void **)&buf, block_size, MAX_POSIX_MEMALIGN_SIZE)) {
       perror("posix_memalign");
       exit(EXIT_FAILURE);
