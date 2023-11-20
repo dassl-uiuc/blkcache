7fb4c44d17713ab2bb50f41667895d61dbef9b8e
maknee <zhu109@illinois.edu>
2023-11-20 21:07:10 +0000

    Test disabling fsync

--- db/block_db.h
+++ db/block_db.h
@@ -20,8 +20,8 @@ public:
     DB::init(block_cache_config);
 
     fd = open(block_cache_config.db.block_db.filename.c_str(),
-              O_CREAT | O_RDWR | O_TRUNC | O_DIRECT | O_SYNC, S_IRWXU);
-
+              O_CREAT | O_RDWR | O_TRUNC | O_DIRECT, S_IRWXU);
+    // | O_SYNC
     if (fd == -1) {
       perror("open");
       exit(EXIT_FAILURE);
