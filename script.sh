7a92384a8cfd3123ada1dbd0356845e161332134
shreesha00 <shreeshagbhat@gmail.com>
2023-12-21 02:59:10 -0700

    Added simple script to run local-cache experiments

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Iterate over cache percentages in the range of 10 to 100 with a step of 10
-for cache_percentage in {10..100..10}
+for cache_percentage in {0..100..10}
 do
     echo "Running for $cache_percentage"
-    sudo ./writer 10000
-    echo 3 > /proc/sys/vm/drop_caches
-    sudo ./reader $cache_percentage 10000
+    sudo ./writer 10000 /dev/sdb
+    sudo ./reader $cache_percentage 10000 /dev/sdb
 done
