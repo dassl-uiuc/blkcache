15d17a124f4c76a07b751c5beafce714f48b0b0e
KSTARK007 <kiranhombal98@gmail.com>
2024-04-10 11:30:56 -0600

    changed access rate to evaluate

--- cache_policy/access_rate_policy.h
+++ cache_policy/access_rate_policy.h
@@ -49,7 +49,7 @@ public:
   bool put_access_rate_match(const KeyType &key, const ValueType &val,
            bool owning = false) override {
     update_frequency(key);
-    if(get_frequency(key) >= 10000){
+    if(get_frequency(key) >= 25){
       info("Access rate match for key: {}", key);
       put(key, val, owning);
       return true;
@@ -60,7 +60,7 @@ public:
   ValueType get(const KeyType &key) override {
     String skey(key.c_str(), key.length());
     uint64_t current_accesses = total_accesses.fetch_add(1, std::memory_order_relaxed) + 1;
-    if(current_accesses > 10000000){
+    if(current_accesses > 1000000){
       std::lock_guard<std::mutex> lock(key_freq_mutex);
         if (total_accesses.load(std::memory_order_relaxed) >= 10000000) {
           info("Clearing frequency");
