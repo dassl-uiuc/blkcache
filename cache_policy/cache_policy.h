c3d80137e5821bb59f3a07eac7da577f99a1ecac
Maknee <zhu109@illinois.edu>
2024-04-11 16:22:09 -0600

    E

--- cache_policy/cache_policy.h
+++ cache_policy/cache_policy.h
@@ -10,6 +10,9 @@
 #include "thread_safe_lru/common.h"
 
 // RDMA related, wrong to be put here but oh well
+#define CACHE_INDEX_INVALID (uintptr_t)-1;
+constexpr RDMACacheIndex InvalidRDMACacheIndex{ CACHE_INDEX_INVALID, false, 0 };
+#define KEY_VALUE_PTR_INVALID -1
 
 struct KeyValue
 {
@@ -38,7 +41,7 @@ struct RDMAKeyValueStorage
     key_value_buffer_size = 1024 * 1024 * 1024;
 
     key_value_buffer = std::malloc(key_value_buffer_size);
-    std::memset(key_value_buffer, -1, key_value_buffer_size);
+    std::memset(key_value_buffer, KEY_VALUE_PTR_INVALID, key_value_buffer_size);
     key_value_mbr = std::make_unique<std::pmr::monotonic_buffer_resource>(key_value_buffer, key_value_buffer_size);
     key_value_upr = std::make_unique<std::pmr::unsynchronized_pool_resource>(key_value_mbr.get());
     key_value_pa = std::make_unique<std::pmr::polymorphic_allocator<uint8_t>>(key_value_upr.get());
@@ -67,7 +70,7 @@ struct RDMAKeyValueStorage
   {
     auto size = get_allocated_cache_index_size();
     auto buffer = reinterpret_cast<RDMACacheIndex*>(std::malloc(size));
-    std::memset(buffer, -1, size);
+    std::memset(buffer, CACHE_INDEX_INVALID, size);
     return buffer;
   }
 
@@ -95,9 +98,9 @@ struct RDMAKeyValueStorage
 
   void deallocate(KeyValue key_value)
   {
-    cache_index_buffer[*key_value.key] = RDMACacheIndex{ -1 };
+    cache_index_buffer[*key_value.key] = InvalidRDMACacheIndex;
     // info("DEALLOC {} {}", (void*)key_value.key, (uint64_t)*key_value.key);
-    *key_value.key = -1;
+    *key_value.key = KEY_VALUE_PTR_INVALID;
     key_value_pa->deallocate((uint8_t*)key_value.key, get_key_value_size());
   }
 
