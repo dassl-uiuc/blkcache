e3da6f88a73e0f6c09dc80d012ab44d7fd9979b5
makneee <makneee@node14.large-c-2.rasl-pg0.utah.cloudlab.us>
2024-04-26 16:49:52 -0600

    If other nodes don't have, singleton evict

--- thread_safe_lru/nchance-cache.h
+++ thread_safe_lru/nchance-cache.h
@@ -577,7 +577,7 @@ evict() {
     // info("[Singleton to forward]");
     return static_cast<void*>(data);
   } else {
-    if (replicaCount > 1) {
+    if (replicaCount <= 1) {
       // info("[Singleton to forward]");
       return static_cast<void*>(data);
     }
