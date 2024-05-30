041db6d648b4291e519ab15ca09cb26a58aab160
makneee <makneee@node0.ldc-nodes-2.rasl-pg0.utah.cloudlab.us>
2024-05-30 16:06:53 -0600

    Block db

--- db/block_db.h
+++ db/block_db.h
@@ -121,10 +121,10 @@ public:
         
         // Init ring
         io_uring_params params{};
-        params.flags |= IORING_SETUP_SQPOLL;
-        params.sq_thread_idle = 2000;
+        // params.flags |= IORING_SETUP_SQPOLL;
+        // params.sq_thread_idle = 2000;
         io_uring_queue_init_params(block_cache_config.db.block_db.io_uring_ring_size, &iouring_worker->ring, &params);
-        io_uring_register_files(&iouring_worker->ring, &fd, 1); // required for sq polling
+        // io_uring_register_files(&iouring_worker->ring, &fd, 1); // required for sq polling
 
         // Init read requests
         for (auto i = 0; i < block_cache_config.db.block_db.io_uring_ring_size; i++)
