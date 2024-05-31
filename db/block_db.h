dad8c46d1e967d527dffda0756fcfcb796fcf109
makneee <makneee@node0.ldc-nodes-2.rasl-pg0.utah.cloudlab.us>
2024-05-31 09:08:37 -0600

    DB

--- db/block_db.h
+++ db/block_db.h
@@ -277,7 +277,7 @@ public:
         {
           bool batch_writes = true;
           auto batch_write_current_size = 0;
-          auto batch_write_size = 256;
+          auto batch_write_size = 64;
           while (!g_stop)
           {
             AsyncRequest async_request;
