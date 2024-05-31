2e5e4b69ec8cfe20bbd32f443346ad8fcb56a609
makneee <makneee@node0.ldc-nodes-2.rasl-pg0.utah.cloudlab.us>
2024-05-31 11:51:14 -0600

    write db

--- db/block_db.h
+++ db/block_db.h
@@ -148,7 +148,7 @@ public:
       
       // Init ring
       io_uring_params params{};
-      // params.flags |= IORING_SETUP_IOPOLL;
+      params.flags |= IORING_SETUP_IOPOLL;
       // params.flags |= IORING_SETUP_SINGLE_ISSUER | IORING_SETUP_DEFER_TASKRUN;
       // params.flags |= IORING_SETUP_SQPOLL;
       // params.sq_thread_idle = 2000;
