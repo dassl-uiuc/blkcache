7300e785fa22737c03698d759bc81c79adb6e072
makneee <makneee@node0.ldc-nodes-2.rasl-pg0.utah.cloudlab.us>
2024-06-02 14:48:34 -0600

    block on pending writes

--- db/block_db.h
+++ db/block_db.h
@@ -677,27 +677,13 @@ public:
         uint64_t submit_write_id = submitted_async_write_id.load(std::memory_order::relaxed);
         uint64_t waited_write_id = waited_async_write_id.load(std::memory_order::relaxed);
 
-        if (batch_write_size > 0)
+        if (waited_write_id - submit_write_id > batch_max_pending_requests)
         {
-          if (waited_write_id - submit_write_id > batch_max_pending_requests * batch_write_size)
-          {
-            std::this_thread::yield();
-          }
-          else
-          {
-            break;
-          }
+          std::this_thread::yield();
         }
         else
         {
-          if (waited_write_id - submit_write_id > batch_max_pending_requests)
-          {
-            std::this_thread::yield();
-          }
-          else
-          {
-            break;
-          }
+          break;
         }
       }
     }
