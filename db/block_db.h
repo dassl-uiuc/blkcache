347e0dd1b5b2d0106da1cf418a2475e553f1d864
makneee <makneee@node0.ldc-nodes-2.rasl-pg0.utah.cloudlab.us>
2024-06-02 14:49:14 -0600

    block on pending writes

--- db/block_db.h
+++ db/block_db.h
@@ -661,6 +661,7 @@ public:
     io_uring_prep_writev(sqe, fd, iovecs.data(), IO_VEC_DEFAULT_SIZE, offset);
     io_uring_sqe_set_data(sqe, async_read_write_request);
     io_uring_submit(&iouring_worker->ring);
+    submitted_async_write_id.fetch_add(1, std::memory_order::relaxed);
 #endif
 
     return id;
