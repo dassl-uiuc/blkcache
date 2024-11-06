03203a568b5d4e871b18f8c1a1d20dea488d1635
Tony Hong <64179036+tonyhong007@users.noreply.github.com>
2024-11-05 23:01:53 -0600

    Update config.h

--- config.h
+++ config.h
@@ -178,6 +178,7 @@ inline void from_json(const json &j, Baseline &baseline) {
 
 inline void from_json(const json &j, BlockCacheConfig &bcc) {
   j.at("ingest_block_index").get_to(bcc.ingest_block_index);
+  j.at("craq_enabled").get_to(bcc.craq_enabled);
   j.at("policy_type").get_to(bcc.policy_type);
   j.at("rdma_port").get_to(bcc.rdma_port);
   j.at("db_type").get_to(bcc.db_type);
